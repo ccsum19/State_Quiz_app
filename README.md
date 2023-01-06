ed52c488465b158df5f3ab5a38967806daeced6a
Sumin Cho <suminyj@sogang.ac.kr>
2023-01-06 06:59:44 -0500

    message

---
+++ README.md
@@ -0,0 +1,20 @@
+##  Quiz Application ## 
+* An interactive quiz application which shows 5 questions on the interface, one by one.
+* Questions are retrieved randomly from SQLite database. (The database is scalable. Can
+  add any no of questions. Currently the app contains 50 questions)
+* Calculates the total score and shows the result at the end.
+
+*Learn from this web: https://www.javatpoint.com/android-sqlite-tutorial
+
+What's new
+
+1. Add score
+2. Add highscore
+3. Number of question will display result
+4. Using shared preference to keep value
+
+
+
+#### Screenshots ####
+
+![Alt text](quizapp.png?raw=true "Title")
---
+++ app/src/main/java/com/example/josethomas/quizapp/ResultActivity.java
@@ -0,0 +1,60 @@
+package com.example.josethomas.quizapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import static com.example.josethomas.quizapp.MainActivity.result;
+import static com.example.josethomas.quizapp.MainActivity.score;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        TextView scoreTxtView = (TextView) findViewById(R.id.score);
+        RatingBar ratingBar = (RatingBar)findViewById(R.id.ratingBar1);
+        ImageView img = (ImageView)findViewById(R.id.img1);
+
+
+         SharedPreferences sharedPreferences = getSharedPreferences("Result", Context.MODE_PRIVATE);
+        int score1 = sharedPreferences.getInt(result, score);
+
+//
+//        Bundle b = getIntent().getExtras();
+//        int score = b.getInt("score");
+        ratingBar.setRating(score1);
+        scoreTxtView.setText(String.valueOf(score1));
+
+        if(score1 == 0){
+            img.setImageResource(R.drawable.score_0);
+        }else if(score1 == 1){
+            img.setImageResource(R.drawable.score_1);
+        }else if(score1 == 2){
+            img.setImageResource(R.drawable.score_2);
+        }else if(score1 == 3){
+            img.setImageResource(R.drawable.score_3);
+        }else if(score1 == 4){
+            img.setImageResource(R.drawable.score_4);
+        }else if(score1 == 5){
+            img.setImageResource(R.drawable.score_5);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        Intent intent = new Intent(ResultActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
